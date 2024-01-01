11d13bb7903bb47158b584e8d276005eb64b0ddb
sanjay-124 <mrsanjay2709@gamil.com>
2024-01-01 21:52:53 +0530

    database added to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Feedback from './Feedback.jsx'
+export default function Apps() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/feedback" element={<Feedback/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Apps />);
