2ff77f41ba4908ec7a00f8b7063a56f5f36c7dbe
sanjay-124 <mrsanjay2709@gamil.com>
2024-01-01 17:35:29 +0530

    Shravan is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <div>
-        <h1 className='font-bold text-red-700'>
-HEY
-        </h1>
-      </div>
-  )
+    <div>
+      <h1 className="font-bold text-red-700">HEY</h1>
+      <h2>SHARAVAN</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
