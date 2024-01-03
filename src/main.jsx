aea8b11b7c1aebb13f2b9c6e7fcd94ee0182aafa
sanjay-124 <mrsanjay2709@gamil.com>
2024-01-04 02:47:49 +0530

    routes fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from "./App.jsx";
 import "./index.css";
 import Feedback from "./pages/feedback.jsx";
